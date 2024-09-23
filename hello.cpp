79539be513b02d09a53e7e7637ecf80dbc414244
alizasolangi <alizasolangi67@gmail.com>
2024-09-23 00:29:00 -0700

    Adding file and assignment

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b; 
+	cout<<"hello\n";
+	cout<<"enter a number ";
+	cin>>a;
+	cout<<"\nenter a number ";
+	cin>>b;
+	cout<<"\nsum of two numbers is "<<a+b;
+	return 0;
+	
+
+} 
